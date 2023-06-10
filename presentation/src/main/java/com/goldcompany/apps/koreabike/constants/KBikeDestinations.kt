8f80ee3ca480d8771438ef19e9c2b611d665089a
JunghunKum <123260423+kumgold@users.noreply.github.com>
2023-06-10 14:44:18 +0900

    Update KBikeDestinations.kt

--- presentation/src/main/java/com/goldcompany/apps/koreabike/constants/KBikeDestinations.kt
+++ presentation/src/main/java/com/goldcompany/apps/koreabike/constants/KBikeDestinations.kt
@@ -1,7 +1,6 @@
 package com.goldcompany.apps.koreabike.constants
 
 object KBikeDestinations {
-    const val HOME = "home"
     const val BIKE_MAP_SCREEN = "bike_map"
     const val SEARCH_PLACE_SCREEN = "search_place"
     const val MY_PLACE_SCREEN = "my_place"
